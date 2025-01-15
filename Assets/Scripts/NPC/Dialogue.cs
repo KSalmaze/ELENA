4c6e6db3ae98b329a467d68ceaf4aabedd9e8734
KSalmaze <pedrohsalmaze@gmail.com>
2025-01-15 13:55:47 -0300

    Updating some packages

--- Assets/Scripts/NPC/Dialogue.cs
+++ Assets/Scripts/NPC/Dialogue.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class Dialogue : MonoBehaviour
 {
+    [SerializeField] private char separationCharacter = '%';
+    
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
