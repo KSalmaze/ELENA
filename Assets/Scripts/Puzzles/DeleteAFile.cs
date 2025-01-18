8849b5406822dc674a140b60f5b5ac729385a7a0
KSalmaze <pedrohsalmaze@gmail.com>
2025-01-17 23:53:38 -0300

    Puzzle 1

--- Assets/Scripts/Puzzles/DeleteAFile.cs
+++ Assets/Scripts/Puzzles/DeleteAFile.cs
@@ -1,24 +1,45 @@
-using Puzzles;
+using System.Collections;
+using System.IO;
 using UnityEngine;
 
-public class DeleteAFile : MonoBehaviour
+namespace Puzzles
 {
-    [SerializeField] private FileManager fileManager;
-    [SerializeField] private Dialogue npc;
-    [SerializeField] private Variables variables;
+    public class DeleteAFile : MonoBehaviour
+    {
+        [SerializeField] private FileManager fileManager;
+        [SerializeField] private Dialogue npc;
+        [SerializeField] private Variables variables;
 
-    [SerializeField] private string fileFolder;
-    [SerializeField] private string fileName;
+        [SerializeField] private string fileFolder;
+        [SerializeField] private string fileName;
     
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        variables.SetVariable("arq1", fileManager.currentDirectory + "/" + fileFolder);
-    }
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+            variables.SetVariable("arq1", fileManager.gameplayDirectory + "/" + fileFolder);
+            string file = fileManager.gameplayDirectory + "/" + fileFolder + "/" + fileName;
+            Debug.Log($"Verficiar aquivo: {file}");
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public void CheckForFileDelete()
+        {
+            StartCoroutine(MonitorarArquivo());
+        }
+    
+        private IEnumerator MonitorarArquivo()
+        {
+            string filePath = fileManager.gameplayDirectory + "/" + fileFolder + "/" + fileName;
+            
+            while (true)
+            {
+                if (!File.Exists(filePath))
+                {
+                    npc.GoToNextDialogue();
+                    yield break;
+                }
+
+                yield return new WaitForSeconds(0.5f);
+            }
+        }
     }
 }
